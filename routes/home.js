350791d63e1ad78c76d9e19fec91f905d4308729
Piradeep2409 <piradeep24sep@gmail.com>
2025-06-23 13:33:23 +0530

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.get("/com.apple.remotemanagement", (req, res) => {
   console.log('Headers:', req.headers);
 
-  const responseText = '{"Servers":[{"Version":"mdm-byod","BaseURL":"https://mdmqa-w11-2.csez.zohocorpin.com:9383/mdm/client/v1/enroll?encapiKey=2522287b&templateToken=a43239406fa3286d1fd77f2608db9ad8"}]}'
+  const responseText = '{"Servers":[{"Version":"mdm-byod","BaseURL":"https://piradeep-22252:9383/mdm/client/v1/enroll?encapiKey=2522287b&templateToken=a43239406fa3286d1fd77f2608db9ad8"}]}'
   const inputStream = Buffer.from(responseText);
 
   const responseHeaders = {
